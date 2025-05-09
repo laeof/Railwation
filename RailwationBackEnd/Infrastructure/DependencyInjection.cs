1b24d60862fadb0ac9ec6a19cf2441d9b227f8cb
laoef <turututuuuuuuu@gmail.com>
2025-05-09 16:52:32 +0300

    environments

--- RailwationBackEnd/Infrastructure/DependencyInjection.cs
+++ RailwationBackEnd/Infrastructure/DependencyInjection.cs
@@ -17,17 +17,17 @@ public static class DependencyInjection
 
         services.AddScoped<IIntegrationService, IntegrationService>();
 
-        //services.AddDbContext<AppDbContext>(options =>
-        //{
-        //    options.UseNpgsql(
-        //        $"Host={Environment.GetEnvironmentVariable("ASPNETCORE_DB_SERVER")};" +
-        //        $"Port={Environment.GetEnvironmentVariable("ASPNETCORE_DB_PORT")};" +
-        //        $"Username={Environment.GetEnvironmentVariable("ASPNETCORE_DB_USER")};" +
-        //        $"Password={Environment.GetEnvironmentVariable("ASPNETCORE_DB_PASS")};" +
-        //        $"Database={Environment.GetEnvironmentVariable("ASPNETCORE_DB_NAME")};");
-        //});
+        services.AddDbContext<AppDbContext>(options =>
+        {
+           options.UseNpgsql(
+               $"Host={Environment.GetEnvironmentVariable("ASPNETCORE_DB_SERVER")};" +
+               $"Port={Environment.GetEnvironmentVariable("ASPNETCORE_DB_PORT")};" +
+               $"Username={Environment.GetEnvironmentVariable("ASPNETCORE_DB_USER")};" +
+               $"Password={Environment.GetEnvironmentVariable("ASPNETCORE_DB_PASS")};" +
+               $"Database={Environment.GetEnvironmentVariable("ASPNETCORE_DB_NAME")};");
+        });
 
-         services.AddDbContext<AppDbContext>(x => x.UseNpgsql("Host=127.0.0.1;Port=5432;Username=postgres;Password=bt7iC4nN07T0f1nDmyp4ss;Database=railwation"));
+        //  services.AddDbContext<AppDbContext>(x => x.UseNpgsql("Host=127.0.0.1;Port=5432;Username=postgres;Password=bt7iC4nN07T0f1nDmyp4ss;Database=railwation"));
 
         return services;
     }
